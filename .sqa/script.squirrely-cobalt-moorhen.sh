6d52134cde2bf3fd990561f6e1469cd593739ba0
orviz <orviz@ifca.unican.es>
2023-06-20 13:05:50 +0200

    Add JePL file structure

--- .sqa/script.squirrely-cobalt-moorhen.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/madgeek-arc/resource-catalogue &&
-    hadolint */Dockerfile* --failure-threshold error
-)
---
+++ .sqa/script.stinky-sepia-monkey.sh
@@ -0,0 +1,4 @@
+(
+cd github.com/madgeek-arc/resource-catalogue &&
+    hadolint Dockerfile eic-registry/Dockerfile --failure-threshold error
+)
